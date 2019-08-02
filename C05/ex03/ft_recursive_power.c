e9308b5a8bf61e2bb96e257e7fe50658ee46de4c
Julie Carra <julie.carra@live.fr>
2019-08-03 00:04:12 +0200

    Initial commit

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jucarra <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/15 10:21:47 by jucarra           #+#    #+#             */
+/*   Updated: 2019/07/15 10:23:47 by jucarra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	i;
+
+	i = nb;
+	if (power < 0)
+	{
+		return (0);
+	}
+	else if (power == 0)
+	{
+		return (1);
+	}
+	else if (power > 0)
+	{
+		return (nb * ft_recursive_power(nb, power - 1));
+	}
+	return (nb);
+}
---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jucarra <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/15 10:24:36 by jucarra           #+#    #+#             */
+/*   Updated: 2019/07/17 09:38:15 by jucarra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+	{
+		return (-1);
+	}
+	else if (index == 0)
+	{
+		return (0);
+	}
+	else if (index == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+	}
+	return (0);
+}
